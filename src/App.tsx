d5c0f7f4a7e9ec472ecf8dee9a9a19b1a32a4c58
Ivan Isayenko <ivanisayenko@gmail.com>
2024-10-02 09:36:13 +0200

    feat: union of the components to create the app functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { Layout } from "./layout/Layout";
+import { InterfaceInput } from "./components/interface-input/InterfaceInput";
+import { useState } from "react";
+import { InterfaceManagement } from "./components/interface-management/InterfaceManagement";
+import { ParsedInterfaces } from "./utils/generatorUtils";
+import { InterfaceGenerator } from "./components/interface-generator/InterfaceGenerator";
+import { Welcome } from "./components/welcome-component/Welcome";
+
 function App() {
-  return <div>Hello!</div>;
+  const [interfaceStringCode, setInterfaceStringCode] = useState("");
+  const [managedInterface, setManagedInterface] = useState<
+    ParsedInterfaces | undefined
+  >();
+
+  return (
+    <Layout>
+      <Welcome />
+      <div className="flex flex-row gap-6 flex-wrap justify-center">
+        <div className="basis-2/5">
+          <InterfaceInput
+            onCodeChange={(code) => setInterfaceStringCode(code)}
+          />
+        </div>
+        <div className="basis-2/6">
+          <InterfaceGenerator managedInterface={managedInterface} />
+        </div>
+      </div>
+      <div className="basis-3/6 mt-16">
+        <InterfaceManagement
+          codeString={interfaceStringCode}
+          onInterfaceManage={(generateInterfaceFrom) =>
+            setManagedInterface(generateInterfaceFrom)
+          }
+        />
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
